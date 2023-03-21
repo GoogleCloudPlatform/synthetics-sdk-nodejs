d341d2b3bf2a9bd374e65909a159a4028e0043ac
Daniel Koss <dkoss@google.com>
2023-03-21 13:08:31 -0400

    Adds runtime metadata extractor module for pulling environment variables relevant to the synthetics product
    Change-Id: I45cdb69c999a256b4093255139ef0577d6922ee3

---
+++ src/runtime_metadata_extractor.ts
@@ -0,0 +1,50 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+let runtime_metadata: { [key: string]: string } = {};
+const list_of_env_variables = [
+  'K_SERVICE',
+  'K_REVISION',
+  'K_CONFIGURATION',
+  'npm_package_version',
+  'npm_package_name',
+];
+
+/**
+ * NOTE: This funciton should only be called inside of test files, or within
+ * this file directly.
+ * This function repopulates the global runtime_metadata object.
+ */
+export function reloadMetadata() {
+  runtime_metadata = {};
+
+  list_of_env_variables.forEach((env_variable) => {
+    const env_variable_value = process.env[env_variable];
+    if (env_variable_value) {
+      runtime_metadata[env_variable] = env_variable_value;
+    }
+  });
+}
+
+reloadMetadata();
+
+/**
+ * Retrieves all runtime metadata relevant to GCM Synthetics and Cloud
+ * Monitoring
+ * @return {Object.<string, number>} Runtime metadata relevant to Cloud
+ *                                   Monitoring
+ */
+export function getRuntimeMetadata() {
+  return runtime_metadata;
+}
---
+++ test/unit/runtime_metadata_extractor.spec.ts
@@ -0,0 +1,57 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { expect } from 'chai';
+
+import {
+  getRuntimeMetadata,
+  reloadMetadata,
+} from '../../src/runtime_metadata_extractor';
+
+describe('runtimeMetadata', () => {
+  const env = process.env;
+
+  afterEach(() => {
+    process.env = env;
+  });
+
+  it('loads all runtime metadata', () => {
+    process.env = {
+      K_SERVICE: 'service_name',
+      K_REVISION: 'service_revision',
+      K_CONFIGURATION: 'configuration',
+      npm_package_version: '1.0.0',
+      npm_package_name: 'the package name',
+      other: 'fields',
+      that: 'dont',
+      matter: 'at all',
+    };
+    reloadMetadata();
+
+    expect(getRuntimeMetadata()).to.deep.equal({
+      K_SERVICE: 'service_name',
+      K_REVISION: 'service_revision',
+      K_CONFIGURATION: 'configuration',
+      npm_package_version: '1.0.0',
+      npm_package_name: 'the package name',
+    });
+  });
+
+  it('sets only metadata thats present and relevant', () => {
+    process.env = { other: 'fields', that: 'dont', matter: 'at all' };
+    reloadMetadata();
+
+    expect(getRuntimeMetadata()).to.deep.equal({});
+  });
+});
