2f145150e33ecb045d8709c025a255fed700445d
Daniel Koss <dkoss@google.com>
2023-03-20 22:43:33 -0400

    Creates a mocha reporter for GCM Synthetictics reporter that writes the output of a mocha test in the format of a MochaResultV1 to a given file
    Change-Id: I3ef22cf2b2f4b12c2a4fbd18f4d2f84f2c6cc139

---
+++ src/gcm_synthetics_mocha_reporter.ts
@@ -0,0 +1,222 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as fs from 'fs';
+import * as Mocha from 'mocha';
+import * as path from 'path';
+
+import {
+  MochaResultV1,
+  MochaSuiteResult,
+  MochaTestResult,
+  MochaTestResult_TestError_StackFrame,
+} from './proto/synthetic_response';
+
+const {
+  EVENT_RUN_END,
+  EVENT_TEST_BEGIN,
+  EVENT_TEST_PASS,
+  EVENT_TEST_FAIL,
+  EVENT_SUITE_BEGIN,
+  EVENT_TEST_PENDING,
+  EVENT_RUN_BEGIN,
+} = Mocha.Runner.constants;
+
+interface GcmSyntheticsReporterOptions {
+  reporterOption: { output: string | null };
+}
+
+class GcmSyntheticsReporter {
+  /**
+   * Constructs a new `GcmSyntheticsReporter` reporter instance.
+   *
+   * @public
+   * @class GcmSyntheticsReporter
+   * @memberof Mocha.reporters
+   * @param {!Runner} runner - Instance triggers reporter actions.
+   * @param {?Object=} options - runner options
+   * @param {?string=} options.output - If not provided, output is logged to
+   *                   stdout. If provided, file location location where output
+   *                   is written.
+   */
+  constructor(runner: Mocha.Runner, options: GcmSyntheticsReporterOptions) {
+    const output = options?.reporterOption?.output;
+
+    const suiteResults: MochaSuiteResult = {
+      suite_count: 0,
+      test_count: 0,
+      passing_test_count: 0,
+      pending_test_count: 0,
+      failing_test_count: 0,
+      suite_start_time: '',
+      suite_end_time: '',
+    };
+    const testResults: MochaTestResult[] = [];
+
+    runner
+      .on(EVENT_RUN_BEGIN, () => {
+        suiteResults.suite_start_time = new Date().toISOString();
+      })
+      .on(EVENT_SUITE_BEGIN, (suite: Mocha.Suite) => {
+        // Only add root suite when it has tests directly in it.
+        if (!suite.root || suite.tests.length > 0) {
+          suiteResults.suite_count = (suiteResults.suite_count ?? 0) + 1;
+        }
+      })
+      .on(EVENT_TEST_BEGIN, () => {
+        suiteResults.test_count = (suiteResults.test_count ?? 0) + 1;
+      })
+      .on(EVENT_TEST_PASS, (test: Mocha.Test) => {
+        testResults.push(serializeTest(test, undefined));
+        suiteResults.passing_test_count =
+          (suiteResults.passing_test_count ?? 0) + 1;
+      })
+      .on(EVENT_TEST_FAIL, (test: Mocha.Test, err: Error) => {
+        testResults.push(serializeTest(test, err));
+        suiteResults.failing_test_count =
+          (suiteResults.failing_test_count ?? 0) + 1;
+      })
+      .on(EVENT_TEST_PENDING, () => {
+        suiteResults.pending_test_count =
+          (suiteResults.pending_test_count ?? 0) + 1;
+      })
+      .on(EVENT_RUN_END, () => {
+        suiteResults.suite_end_time = new Date().toISOString();
+        const mochaResult: MochaResultV1 = {
+          suite_result: suiteResults,
+          test_results: testResults,
+        };
+        const json = JSON.stringify(MochaResultV1.toJSON(mochaResult), null, 2);
+        if (output) {
+          try {
+            fs.mkdirSync(path.dirname(output), { recursive: true });
+            fs.writeFileSync(output, json);
+          } catch (err: unknown) {
+            const message = err instanceof Error ? err.message : '';
+            process.stderr.write(
+              `[mocha] writing output to '${output}' failed: ${message}\n`
+            );
+            process.stdout.write(`${json}\n`);
+          }
+        } else {
+          process.stdout.write(`${json}\n`);
+        }
+      });
+  }
+}
+
+/**
+ * Serializes test results to relevant results for uptime to process.
+ * @param {!Test} test - Test that was ran by mocha, pass & duration stats
+ *     being
+ *                      relevant.
+ * @param {?Error} err - Error thrown when a test fails.
+ * @return {!Object} Serialized results relevant for reporting by uptime.
+ */
+export function serializeTest(
+  test: Mocha.Test,
+  err: NodeJS.ErrnoException | undefined
+): MochaTestResult {
+  const now = Date.now();
+  return {
+    // Relevant for current uptime metrics
+    test_passed: !err,
+    test_start_time: new Date(now - (test.duration ?? 0)).toISOString(),
+    test_end_time: new Date(now).toISOString(),
+    // Additional metadata for logging
+    title: test.title,
+    title_paths: test.titlePath(),
+    error: err
+      ? {
+          name: err.name,
+          message: err.message,
+          stack_frames: serializeStack(err.stack || ''),
+        }
+      : undefined,
+  };
+}
+
+/**
+ * Serializes a NodeJs error stack into a collection of consumable objects.
+ * @param {string} errStack The stack property on a NodeJs Error.
+ * @return {!array} Serialized error stack frames which include properties
+ *                  functionName, fileName, lineNumber, columnNumber
+ */
+function serializeStack(
+  errStack: string
+): MochaTestResult_TestError_StackFrame[] {
+  if (!errStack) {
+    return [];
+  }
+
+  const stack = errStack.split('\n');
+  stack.shift(); // removes first line with redundant error message, already
+  // captured in err.message
+
+  return stack.map((frameStr: string) => {
+    // const stackFrame = {};
+
+    const frame = frameStr.split('at ')[1];
+    if (!frame) {
+      // Frame is in an invalid format, this is defensive.
+      return {};
+    }
+    const locationParansRegex = /\(([^)]+)\)/;
+    const locationInParens = locationParansRegex.exec(frame);
+
+    if (locationInParens) {
+      // if regex passed, location will be found at
+      // [1] index. If so, then there is a function
+      // name prefix
+      return {
+        function_name: frame.split(locationInParens[0])[0].trim(),
+        ...serializeLocation(locationInParens[1]),
+      };
+    }
+    return { function_name: undefined, ...serializeLocation(frame) };
+  });
+}
+
+/**
+ * Serializes the location of an error which takes one of the following forms:
+ * * native, if the frame represents a call internal to V8 (as in [].forEach).
+ * * plain-filename.js:line:column, if the frame represents a call internal to
+ *   Node.js.
+ * * /absolute/path/to/file.js:line:column, if the frame represents a call in
+ * a user program (using CommonJS module system), or its dependencies.
+ * * <transport-protocol>:///url/to/module/file.mjs:line:column, if the frame
+ *   represents a call in a user program (using ES module system), or its
+ *   dependencies.
+ * @param {string} location String location at which an error occurred.
+ * @return {!Object} serializedLocation the serialized location that includes
+ *                   fileName, lineNumber, and columnNumber.
+ */
+function serializeLocation(location: string) {
+  const locationColonSplit = location.split(':'); // last two entries are line, col. There may be
+  // ":" in other places in string.
+  if (locationColonSplit.length < 3) {
+    return { file_name: location };
+  }
+
+  const columnNumber = locationColonSplit.pop();
+  const lineNumber = locationColonSplit.pop();
+
+  return {
+    file_name: locationColonSplit.join(':'),
+    line: Number(lineNumber),
+    column: Number(columnNumber),
+  };
+}
+
+module.exports = GcmSyntheticsReporter;
---
+++ test/unit/gcm_synthetics_mocha_reporter.spec.ts
@@ -0,0 +1,224 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { expect } from 'chai';
+import * as fs from 'fs';
+import * as Mocha from 'mocha';
+import * as path from 'path';
+
+import { MochaResultV1 } from '../../src/proto/synthetic_response';
+
+const reporterPath = path.join(
+  __dirname,
+  '..',
+  '..',
+  'build',
+  'src',
+  'gcm_synthetics_mocha_reporter.js'
+);
+const testResultFile = '/tmp/gcm_synthetics_mocha_reporter.spec.ts/output';
+
+describe('gcm_synthetic_reporter', () => {
+  let mocha: Mocha;
+  let passingTest: Mocha.Test;
+  let failingTest: Mocha.Test;
+  let passingTest2: Mocha.Test;
+  let pendingTest: Mocha.Test;
+
+  beforeEach(() => {
+    mocha = new Mocha({
+      reporter: reporterPath,
+      reporterOptions: { output: testResultFile },
+    });
+
+    passingTest = new Mocha.Test('passing test', () => {});
+    failingTest = new Mocha.Test('failing test', () => {
+      const innerFn = function innerFn() {
+        throw new Error('this test has failed');
+      };
+      innerFn();
+    });
+    passingTest2 = new Mocha.Test('passing test 2', () => {});
+    pendingTest = new Mocha.Test('pending test');
+  });
+
+  const readOutputFile = (): MochaResultV1 => {
+    const output = fs.readFileSync(testResultFile, { encoding: 'utf-8' });
+    fs.unlinkSync(testResultFile);
+    return JSON.parse(output);
+  };
+
+  it('Contains information for a passing test', (done) => {
+    mocha.suite.addTest(passingTest);
+
+    mocha.run(() => {
+      const { suite_result, test_results } = readOutputFile();
+      try {
+        expect(suite_result?.suite_count).to.equal(1);
+        expect(suite_result?.test_count).to.equal(1);
+        expect(suite_result?.passing_test_count).to.equal(1);
+        expect(suite_result?.pending_test_count).to.equal(0);
+        expect(suite_result?.failing_test_count).to.equal(0);
+
+        expect(test_results).to.have.length(1);
+        expect(test_results?.[0]?.test_passed).to.be.true;
+        expect(test_results?.[0]?.title).to.equal('passing test');
+        expect(test_results?.[0]?.title_paths).to.deep.equal(['passing test']);
+
+        expect(test_results?.[0]?.error).to.be.undefined;
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('Reports information for a failing test', (done) => {
+    mocha.suite.addTest(failingTest);
+
+    mocha.run(() => {
+      const { suite_result, test_results } = readOutputFile();
+      try {
+        expect(suite_result?.suite_count).to.equal(1);
+        expect(suite_result?.test_count).to.equal(1);
+        expect(suite_result?.passing_test_count).to.equal(0);
+        expect(suite_result?.pending_test_count).to.equal(0);
+        expect(suite_result?.failing_test_count).to.equal(1);
+
+        expect(test_results).to.have.length(1);
+        expect(test_results?.[0]?.test_passed).to.be.false;
+        expect(test_results?.[0]?.title).to.equal('failing test');
+        expect(test_results?.[0]?.title_paths).to.deep.equal(['failing test']);
+
+        const error = test_results?.[0]?.error;
+        expect(error?.name).to.equal('Error');
+        expect(error?.message).to.equal('this test has failed');
+
+        expect(error?.stack_frames?.[0]?.function_name).to.equal('innerFn');
+        expect(error?.stack_frames?.[0]?.file_name).to.equal(
+          'test/unit/gcm_synthetics_mocha_reporter.spec.ts'
+        );
+        expect(error?.stack_frames?.[0]?.line).to.not.be.undefined;
+        expect(error?.stack_frames?.[0]?.column).to.not.be.undefined;
+
+        expect(error?.stack_frames?.[1]?.function_name).to.equal(
+          'Context.<anonymous>'
+        );
+        expect(error?.stack_frames?.[1]?.file_name).to.equal(
+          'test/unit/gcm_synthetics_mocha_reporter.spec.ts'
+        );
+        expect(error?.stack_frames?.[1]?.line).to.not.be.undefined;
+        expect(error?.stack_frames?.[1]?.column).to.not.be.undefined;
+
+        expect(error?.stack_frames?.[2]?.function_name).to.equal(
+          'processImmediate'
+        );
+        expect(error?.stack_frames?.[2]?.file_name).to.equal(
+          'node:internal/timers'
+        );
+        expect(error?.stack_frames?.[2]?.line).to.not.be.undefined;
+        expect(error?.stack_frames?.[2]?.column).to.not.be.undefined;
+
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+  it('Reports information for a pending test', (done) => {
+    mocha.suite.addTest(pendingTest);
+
+    mocha.run(() => {
+      const { suite_result, test_results } = readOutputFile();
+      try {
+        expect(suite_result?.suite_count).to.equal(1);
+        expect(suite_result?.test_count).to.equal(0);
+        expect(suite_result?.passing_test_count).to.equal(0);
+        expect(suite_result?.pending_test_count).to.equal(1);
+        expect(suite_result?.failing_test_count).to.equal(0);
+
+        expect(test_results).to.have.length(0);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('Reports information for multiple tests', (done) => {
+    mocha.suite.addTest(passingTest);
+    mocha.suite.addTest(failingTest);
+    const subSuite = new Mocha.Suite('sub suite');
+    subSuite.addTest(pendingTest);
+    subSuite.addTest(passingTest2);
+    mocha.suite.addSuite(subSuite);
+
+    mocha.run(() => {
+      const { suite_result, test_results } = readOutputFile();
+      try {
+        expect(suite_result?.suite_count).to.equal(2);
+        expect(suite_result?.test_count).to.equal(3);
+        expect(suite_result?.passing_test_count).to.equal(2);
+        expect(suite_result?.pending_test_count).to.equal(1);
+        expect(suite_result?.failing_test_count).to.equal(1);
+
+        expect(test_results).to.have.length(3);
+
+        expect(test_results?.[0]?.test_passed).to.be.true;
+        expect(test_results?.[0]?.title).to.equal('passing test');
+        expect(test_results?.[0]?.title_paths).to.deep.equal(['passing test']);
+
+        expect(test_results?.[1]?.test_passed).to.be.false;
+        expect(test_results?.[1]?.title).to.equal('failing test');
+        expect(test_results?.[1]?.title_paths).to.deep.equal(['failing test']);
+
+        expect(test_results?.[2]?.test_passed).to.be.true;
+        expect(test_results?.[2]?.title).to.equal('passing test 2');
+        expect(test_results?.[2]?.title_paths).to.deep.equal([
+          'sub suite',
+          'passing test 2',
+        ]);
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+
+  it('Reports relatively accurate timings', (done) => {
+    mocha.suite.addTest(failingTest);
+
+    const start = new Date();
+    mocha.run(() => {
+      const end = new Date();
+
+      const { suite_result, test_results } = readOutputFile();
+      const suiteStart = new Date(suite_result?.suite_start_time ?? '');
+      const suiteEnd = new Date(suite_result?.suite_end_time ?? '');
+
+      try {
+        expect(suiteStart >= start).to.be.true;
+        expect(suiteEnd <= end).to.be.true;
+
+        expect(new Date(test_results?.[0].test_start_time ?? '') >= suiteStart)
+          .to.be.true;
+        expect(new Date(test_results?.[0]?.test_end_time ?? '') <= suiteEnd).to
+          .be.true;
+        done();
+      } catch (e) {
+        done(e);
+      }
+    });
+  });
+});
