8cc7463ededa2cbea3bb7c57bfb4589655681b04
Adam Weidman <adamfweidman@google.com>
2023-08-14 16:33:52 -0400

    formatting

--- packages/synthetics-sdk-broken-links/test/unit/link_utils.spec.ts
+++ packages/synthetics-sdk-broken-links/test/unit/link_utils.spec.ts
@@ -12,45 +12,55 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import {expect} from 'chai';
+import { expect } from 'chai';
 import {
   ResponseStatusCode,
   ResponseStatusCode_StatusClass,
 } from '@google-cloud/synthetics-sdk-api';
-import { checkStatusPassing } from "../../src/link_utils"
+import { checkStatusPassing } from '../../src/link_utils';
 
 describe('GCM Synthetics Broken Links', async () => {
-    const success_status_value: ResponseStatusCode = {status_value: 200};
-    const failure_status_value: ResponseStatusCode = {status_value: 404};
-    const status_class_1xx: ResponseStatusCode = {status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_1XX }
-    const status_class_2xx: ResponseStatusCode = {status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_2XX }
-    const status_class_3xx: ResponseStatusCode = {status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_3XX }
-    const status_class_4xx: ResponseStatusCode = {status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_4XX }
-    const status_class_5xx: ResponseStatusCode = {status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_5XX }
+  const success_status_value: ResponseStatusCode = { status_value: 200 };
+  const failure_status_value: ResponseStatusCode = { status_value: 404 };
+  const status_class_1xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_1XX,
+  };
+  const status_class_2xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_2XX,
+  };
+  const status_class_3xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_3XX,
+  };
+  const status_class_4xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_4XX,
+  };
+  const status_class_5xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_5XX,
+  };
 
-    it('checkStatusPassing returns correctly when passed a number as ResponseStatusCode', () => {
-      // expecting success
-      expect(checkStatusPassing(success_status_value, 200)).to.be.true;
-      expect(checkStatusPassing(success_status_value, 404)).to.be.false;
+  it('checkStatusPassing returns correctly when passed a number as ResponseStatusCode', () => {
+    // expecting success
+    expect(checkStatusPassing(success_status_value, 200)).to.be.true;
+    expect(checkStatusPassing(success_status_value, 404)).to.be.false;
 
-      // expecting failure
-      expect(checkStatusPassing(failure_status_value, 200)).to.be.false;
-      expect(checkStatusPassing(failure_status_value, 404)).to.be.true;
-    });
+    // expecting failure
+    expect(checkStatusPassing(failure_status_value, 200)).to.be.false;
+    expect(checkStatusPassing(failure_status_value, 404)).to.be.true;
+  });
 
-    it('checkStatusPassing returns correctly when passed a statusClass as ResponseStatusCode', () => {
-      // expecting success
-      expect(checkStatusPassing(status_class_1xx, 100)).to.be.true;
-      expect(checkStatusPassing(status_class_2xx, 200)).to.be.true;
-      expect(checkStatusPassing(status_class_3xx, 304)).to.be.true;
-      expect(checkStatusPassing(status_class_4xx, 404)).to.be.true;
-      expect(checkStatusPassing(status_class_5xx, 504)).to.be.true;
+  it('checkStatusPassing returns correctly when passed a statusClass as ResponseStatusCode', () => {
+    // expecting success
+    expect(checkStatusPassing(status_class_1xx, 100)).to.be.true;
+    expect(checkStatusPassing(status_class_2xx, 200)).to.be.true;
+    expect(checkStatusPassing(status_class_3xx, 304)).to.be.true;
+    expect(checkStatusPassing(status_class_4xx, 404)).to.be.true;
+    expect(checkStatusPassing(status_class_5xx, 504)).to.be.true;
 
-      // expecting failure
-      expect(checkStatusPassing(status_class_1xx, 200)).to.be.false;
-      expect(checkStatusPassing(status_class_2xx, 404)).to.be.false;
-      expect(checkStatusPassing(status_class_3xx, 200)).to.be.false;
-      expect(checkStatusPassing(status_class_4xx, 200)).to.be.false;
-      expect(checkStatusPassing(status_class_5xx, 200)).to.be.false;
-    });
+    // expecting failure
+    expect(checkStatusPassing(status_class_1xx, 200)).to.be.false;
+    expect(checkStatusPassing(status_class_2xx, 404)).to.be.false;
+    expect(checkStatusPassing(status_class_3xx, 200)).to.be.false;
+    expect(checkStatusPassing(status_class_4xx, 200)).to.be.false;
+    expect(checkStatusPassing(status_class_5xx, 200)).to.be.false;
+  });
 });
