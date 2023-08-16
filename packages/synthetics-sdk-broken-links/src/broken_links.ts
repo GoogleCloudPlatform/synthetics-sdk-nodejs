cd5b142fadf90bcb1a87523d99dfbce6714ed8a9
Adam Weidman <adamfweidman@google.com>
2023-08-15 22:49:17 -0400

    functionality

--- packages/synthetics-sdk-broken-links/src/broken_links.ts
+++ packages/synthetics-sdk-broken-links/src/broken_links.ts
@@ -15,21 +15,189 @@
 // TODO should I use this stuff or redeclare it? anyway to make it less messy?
 import {
   BrokenLinksResultV1_BrokenLinkCheckerOptions,
+  BrokenLinksResultV1_SyntheticLinkResult,
   ResponseStatusCode,
   SyntheticResult,
 } from '@google-cloud/synthetics-sdk-api';
 
-import { checkStatusPassing, setDefaultOptions } from './link_utils';
+import {
+  checkStatusPassing,
+  setDefaultOptions,
+  LinkIntermediate,
+  isHTTPResponse,
+} from './link_utils';
+import puppeteer, { HTTPResponse, Page } from 'puppeteer';
 
 export async function runBrokenLinks(
   options: BrokenLinksResultV1_BrokenLinkCheckerOptions
 ): Promise<SyntheticResult> {
-  // to resolve warnings
+  // START - to resolve warnings while under development
   options;
   checkStatusPassing({ status_value: 200 } as ResponseStatusCode, 200);
+
+  const browser = await puppeteer.launch({ headless: 'new' });
+  const page = await browser.newPage();
+  await checkLink(
+    page,
+    {} as LinkIntermediate,
+    {} as BrokenLinksResultV1_BrokenLinkCheckerOptions
+  );
+  // END - to resolve warnings  while under development
 
   // options object modified directly
   setDefaultOptions(options);
 
+  // PSEUDOCODE
+
+  // create puppeteer.Browser
+  // create puppeteer.Page & navigate to origin_url, w/ origin specific settings
+
+  // scrape origin_url for all links
+  // (shuffle links if necessary and) truncate at link_limit
+
+  // create new page to be used for all scraped links
+  // navigate to each link - LOOP:
+  //          each call to `checkLinks(...)` will return a `SyntheticLinkResult`
+  //          Object added to an array of `followed_links`
+
+  // returned a SyntheticResult with `options`, `followed_links` &
+  // runtimeMetadata
   return {} as SyntheticResult;
+}
+
+async function checkLink(
+  page: Page,
+  link: LinkIntermediate,
+  options: BrokenLinksResultV1_BrokenLinkCheckerOptions
+): Promise<BrokenLinksResultV1_SyntheticLinkResult> {
+  // START - to resolve warnings while under development
+  page;
+  link;
+  options;
+  // END - to resolve warnings  while under development
+
+  // PSEUDOCODE
+
+  // determine expected_status_code—determined here rather than `navigate(...)`
+  // since it will be used later when creating
+  // `BrokenLinksResultV1_SyntheticLinkResult` Object
+
+  // call `navigate(...)`
+  /**
+   * const {
+   *    response: responseOrError,
+   *    passed,
+   *    testing_only,
+   *    link_start_time,
+   *    link_end_time,
+   * } = await navigate(page, link, expected_status_code, options);
+   */
+
+  // Error handling based on `responseOrError` & passed:
+  //    if      `responseOrError` is type `Error` set `error_type` and
+  //            `error_message` in `SyntheticLinkResult` accordingly
+  //    else if `passed === false`. Set error `SYNTHETIC_INCORRECT_STATUS_CODE`
+
+  // return `SynheticLinkResult` with all calculated information
+  return {} as BrokenLinksResultV1_SyntheticLinkResult;
+}
+
+/**
+ * Navigates to a target URL with retries and timeout handling.
+ *
+ * @param page - The Puppeteer Page instance.
+ * @param link - The LinkIntermediate containing the target URL.
+ * @param expected_status_code - The expected HTTP status code.
+ * @param options - The options for navigation and retries.
+ * @returns Information about navigation attempt:
+ *   - `response`: HTTP response or error if navigation fails, or null.
+ *   - `passed`: Boolean indicating if navigation passed per status code.
+ *   - `retriesRemaining`: Remaining retries after attempt. (for testing)
+ *   - `link_start_time`: Start time of navigation attempt.
+ *   - `link_end_time`: End time of navigation attempt.
+ */
+export async function navigate(
+  page: Page,
+  link: LinkIntermediate,
+  expected_status_code: ResponseStatusCode,
+  options: BrokenLinksResultV1_BrokenLinkCheckerOptions
+): Promise<{
+  response: HTTPResponse | Error | null;
+  passed: boolean;
+  retriesRemaining: number;
+  link_start_time: string;
+  link_end_time: string;
+}> {
+  let link_start_time = '';
+  let link_end_time = '';
+  let retriesRemaining = options.max_retries!;
+  const per_link_timeout_millis =
+    options.per_link_options[options.origin_url]?.link_timeout_millis ||
+    options.link_timeout_millis!;
+
+  let response: HTTPResponse | Error | null = null;
+
+  let passed = false;
+  let used_retry = false;
+  while (retriesRemaining > 0 && !passed) {
+    retriesRemaining--;
+    link_start_time = new Date().toISOString();
+    response = await fetchLink(page, link.target_url, per_link_timeout_millis);
+    link_end_time = new Date().toISOString();
+
+    // prevents errors caused by navigating from one url to same url with a
+    // different anchor part (normally returns null)
+    // e.g. mywebsite.com#heading1 --> mywebsite.com#heading2
+    if (response === null && !used_retry) {
+      link_start_time = new Date().toISOString();
+      await page.goto('about:blank');
+      response = await fetchLink(
+        page,
+        link.target_url,
+        per_link_timeout_millis
+      );
+      used_retry = !used_retry;
+      link_end_time = new Date().toISOString();
+    }
+
+    if (
+      isHTTPResponse(response) &&
+      checkStatusPassing(expected_status_code, response.status())
+    ) {
+      passed = true;
+      return {
+        response,
+        passed,
+        retriesRemaining,
+        link_start_time,
+        link_end_time,
+      };
+    }
+  }
+  return { response, passed, retriesRemaining, link_start_time, link_end_time };
+}
+
+/**
+ * Fetches the target URL using Puppeteer's page.goto method.
+ *
+ * @param page - The Puppeteer Page instance.
+ * @param target_url - The URL to navigate to.
+ * @param timeout - The timeout for the navigation.
+ * @returns The HTTP response, an error if navigation fails, or null if no response.
+ */
+async function fetchLink(
+  page: Page,
+  target_url: string,
+  timeout: number
+): Promise<HTTPResponse | Error | null> {
+  try {
+    const response = await page.goto(target_url, {
+      waitUntil: 'load',
+      timeout: timeout,
+    });
+    return response;
+  } catch (err) {
+    if (err instanceof Error) return err;
+    return null;
+  }
 }
