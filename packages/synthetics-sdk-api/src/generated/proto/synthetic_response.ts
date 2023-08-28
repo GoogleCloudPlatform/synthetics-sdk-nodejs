c7bf1e83a08561377b70e05e830ff30fb3f41fab
Adam Weidman <adamfweidman@google.com>
2023-08-28 11:40:49 -0700

    check link

--- packages/synthetics-sdk-api/src/generated/proto/synthetic_response.ts
+++ packages/synthetics-sdk-api/src/generated/proto/synthetic_response.ts
@@ -1,17 +1,3 @@
-// Copyright 2023 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /* eslint-disable */
 import Long from "long";
 import _m0 from "protobufjs/minimal";
@@ -381,8 +367,9 @@ export interface BrokenLinksResultV1_SyntheticLinkResult {
     | number
     | undefined;
   /**
-   * The class of the error, eg 'connectionaborted', docs:
-   * https://pptr.dev/api/puppeteer.errorcode
+   * 'BrokenLinksSynthetic_IncorrectStatusCode' if the expected and actual
+   * status codes differ. Otherwise, the class of the error thrown, eg
+   * 'connectionaborted', docs: https://pptr.dev/api/puppeteer.errorcode.
    */
   error_type: string;
   /** Error Message, if any */
