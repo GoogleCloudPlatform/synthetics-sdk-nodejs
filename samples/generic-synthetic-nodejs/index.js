801f3228d769669b0fa50116c7b63aa040fdee1e
Tiffany Shen <tishen@google.com>
2023-10-26 17:01:41 -0400

    Updates nodejs and ts templates to incorporate execution id

--- samples/generic-synthetic-nodejs/index.js
+++ samples/generic-synthetic-nodejs/index.js
@@ -20,13 +20,13 @@ const functions = require('@google-cloud/functions-framework');
 const axios = require('axios');
 const assert = require('node:assert');
 
-functions.http('SyntheticFunction', runSyntheticHandler(async ({logger}) => {
+functions.http('SyntheticFunction', runSyntheticHandler(async ({logger, executionId}) => {
   /*
    * This function executes the synthetic code for testing purposes.
    * If the code runs without errors, the synthetic test is considered successful.
    * If an error is thrown during execution, the synthetic test is considered failed.
    */
-  logger.info('Making an http request using synthetics');
+  logger.info('Making an http request using synthetics, with execution id: ' + executionId);
   const url = 'https://www.google.com/'; // URL to send the request to
   return await assert.doesNotReject(axios.get(url));
 }));
