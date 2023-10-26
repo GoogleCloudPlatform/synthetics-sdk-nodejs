80cc318a6547afaab1927c56bbc13f24a03ec7de
tiffunky <113037085+tiffunky@users.noreply.github.com>
2023-10-26 17:29:30 -0400

    chore: updates nodejs and ts samples to incorporate execution id (#75)
    Updates nodejs and ts samples to incorporate execution id

--- samples/generic-synthetic-typescript/src/index.ts
+++ samples/generic-synthetic-typescript/src/index.ts
@@ -21,13 +21,13 @@ import axios from 'axios';
 import assert from 'node:assert';
 import {Logger} from 'winston';
 
-ff.http('SyntheticFunction', runSyntheticHandler(async ({logger}: {logger: Logger}) => {
+ff.http('SyntheticFunction', runSyntheticHandler(async ({logger, executionId}: {logger: Logger, executionId: string|undefined}) => {
   /*
    * This function executes the synthetic code for testing purposes.
    * If the code runs without errors, the synthetic test is considered successful.
    * If an error is thrown during execution, the synthetic test is considered failed.
    */
-  logger.info('Making an http request using synthetics');
+  logger.info('Making an http request using synthetics, with execution id: ' + executionId);
   const url = 'https://www.google.com/'; // URL to send the request to
   return await assert.doesNotReject(axios.get(url));
 }));
