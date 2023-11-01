55376d2d882c23df2e1a3f1c4932f3f5ba761934
Adam Weidman <adamfweidman@google.com>
2023-11-01 14:26:03 -0400

    broken_link_checker

---
+++ packages/synthetics-sdk-broken-links/src/link_utils.ts
@@ -0,0 +1,307 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { HTTPResponse } from 'puppeteer';
+import {
+  BrokenLinksResultV1,
+  BrokenLinksResultV1_BrokenLinkCheckerOptions,
+  BrokenLinksResultV1_BrokenLinkCheckerOptions_LinkOrder,
+  BrokenLinksResultV1_SyntheticLinkResult,
+  GenericResultV1,
+  getRuntimeMetadata,
+  ResponseStatusCode,
+  ResponseStatusCode_StatusClass,
+  SyntheticResult,
+} from '@google-cloud/synthetics-sdk-api';
+
+/**
+ * Represents an intermediate link with its properties.
+ */
+export interface LinkIntermediate {
+  /**
+   * The target URI of the link.
+   */
+  target_uri: string;
+
+  /**
+   * The anchor text of the link.
+   */
+  anchor_text: string;
+
+  /**
+   * The HTML element of the link.
+   */
+  html_element: string;
+}
+
+/**
+ * Represents common response properties for navigation (currently:`fetchLink()`
+ *  and `navigate()`)
+ */
+export interface CommonResponseProps {
+  /**
+   * The response or error received during navigation. Essentially a wrapper
+   * around `page.goto()`.
+   */
+  responseOrError: HTTPResponse | Error | null;
+
+  /**
+   * The start time of the link navigation.
+   */
+  linkStartTime: string;
+
+  /**
+   * The end time of the link navigation.
+   */
+  linkEndTime: string;
+}
+
+/**
+ * Represents the response from a navigation attempt (currently: `navigate()`)
+ */
+export interface NavigateResponse extends CommonResponseProps {
+  /**
+   * Indicates whether the link passed successfully (taking into account per
+   * link options, if present).
+   */
+  passed: boolean;
+
+  /**
+   * The number of navigation retries remaining for the link.
+   */
+  retriesRemaining: number;
+}
+
+/**
+ * Checks if the given status code is passing w.r.t. expected status class or
+ * code
+ *
+ * @param expected - The expected status code.
+ * @param actual - The actual status code.
+ * @returns Whether the status code is passing.
+ */
+export function checkStatusPassing(
+  expected: ResponseStatusCode,
+  actual: number
+): boolean {
+  if (typeof expected?.status_value === 'number') {
+    return expected?.status_value === actual;
+  } else {
+    switch (expected?.status_class) {
+      case ResponseStatusCode_StatusClass.STATUS_CLASS_1XX:
+        return actual >= 100 && actual <= 199;
+      case ResponseStatusCode_StatusClass.STATUS_CLASS_2XX:
+        return actual >= 200 && actual <= 299;
+      case ResponseStatusCode_StatusClass.STATUS_CLASS_3XX:
+        return actual >= 300 && actual <= 399;
+      case ResponseStatusCode_StatusClass.STATUS_CLASS_4XX:
+        return actual >= 400 && actual <= 499;
+      case ResponseStatusCode_StatusClass.STATUS_CLASS_5XX:
+        return actual >= 500 && actual <= 599;
+      default:
+        return false;
+    }
+  }
+}
+
+/**
+ * Type guard function to check if an object is an instance of HTTPResponse.
+ *
+ * @param response - The object to be checked.
+ * @returns `true` if the object is an instance of HTTPResponse, `false` otherwise.
+ */
+export function isHTTPResponse(
+  response: HTTPResponse | Error | null
+): response is HTTPResponse {
+  return (
+    response !== null && typeof response === 'object' && 'status' in response
+  );
+}
+
+/**
+ * Parses an array of followed BrokenLinksResultV1_SyntheticLinkResult's and
+ * aggregates statistics into a single BrokenLinksResultV1 object.
+ *
+ * @param followed_links - An array of BrokenLinksResultV1_SyntheticLinkResult
+ *                         containing link results.
+ * @returns An aggregated BrokenLinksResultV1 containing overall statistics of
+ *          the parsed links.
+ */
+function parseFollowedLinks(
+  followed_links: BrokenLinksResultV1_SyntheticLinkResult[]
+) {
+  const broken_links_result: BrokenLinksResultV1 = {
+    link_count: 0,
+    passing_link_count: 0,
+    failing_link_count: 0,
+    unreachable_count: 0,
+    status2xx_count: 0,
+    status3xx_count: 0,
+    status4xx_count: 0,
+    status5xx_count: 0,
+    options: {} as BrokenLinksResultV1_BrokenLinkCheckerOptions,
+    origin_link_result: {} as BrokenLinksResultV1_SyntheticLinkResult,
+    followed_link_results: [],
+  };
+
+  for (const link of followed_links) {
+    if (link.link_passed === undefined) continue;
+    link.is_origin
+      ? (broken_links_result.origin_link_result = link)
+      : broken_links_result.followed_link_results.push(link);
+
+    broken_links_result.link_count = (broken_links_result.link_count ?? 0) + 1;
+
+    if (link.link_passed) {
+      broken_links_result.passing_link_count =
+        (broken_links_result.passing_link_count ?? 0) + 1;
+    } else {
+      broken_links_result.failing_link_count =
+        (broken_links_result.failing_link_count ?? 0) + 1;
+    }
+
+    switch (Math.floor(link.status_code! / 100)) {
+      case 2:
+        broken_links_result.status2xx_count =
+          (broken_links_result.status2xx_count ?? 0) + 1;
+        break;
+
+      case 3:
+        broken_links_result.status3xx_count =
+          (broken_links_result.status3xx_count ?? 0) + 1;
+        break;
+
+      case 4:
+        broken_links_result.status4xx_count =
+          (broken_links_result.status4xx_count ?? 0) + 1;
+        break;
+
+      case 5:
+        broken_links_result.status5xx_count =
+          (broken_links_result.status5xx_count ?? 0) + 1;
+        break;
+
+      default:
+        // Handle other status codes if needed
+        broken_links_result.unreachable_count =
+          (broken_links_result.unreachable_count ?? 0) + 1;
+        break;
+    }
+  }
+
+  return broken_links_result;
+}
+
+/**
+ * Creates a SyntheticResult object representing the result of a broken link
+ * synthetic execution.
+ *
+ * @param start_time - The start time of the synthetic test in ISO format.
+ * @param options - The BrokenLinkCheckerOptions used for the test.
+ * @param followed_links - An array of BrokenLinksResultV1_SyntheticLinkResult representing followed links.
+ * @returns A SyntheticResult object containing the broken links result, runtime metadata, start time, and end time.
+ */
+export function createSyntheticResult(
+  start_time: string,
+  runtime_metadata: { [key: string]: string },
+  options: BrokenLinksResultV1_BrokenLinkCheckerOptions,
+  followed_links: BrokenLinksResultV1_SyntheticLinkResult[]
+): SyntheticResult {
+  // Create BrokenLinksResultV1 by parsing followed links and setting options
+  const broken_links_result: BrokenLinksResultV1 =
+    parseFollowedLinks(followed_links);
+  broken_links_result.options = options;
+
+  // Create SyntheticResult object
+  const synthetic_result: SyntheticResult = {
+    synthetic_broken_links_result_v1: broken_links_result,
+    runtime_metadata: runtime_metadata,
+    start_time: start_time,
+    end_time: new Date().toISOString(),
+  };
+
+  return synthetic_result;
+}
+
+/**
+ * If the `link_order` is set to `RANDOM`, the links will be shuffled randomly.
+ * Otherwise, the links will be copied without shuffling. Truncate to
+ * `link_limit` regardless
+ *
+ * @param links - The array of links to process.
+ * @param link_limit - The maximum number of links to retain.
+ * @param link_order - Whether or not to shuffle links (enum value).
+ * @returns A new array of links that have been truncated based on the `link_limit`.
+ */
+export function shuffleAndTruncate(
+  links: LinkIntermediate[],
+  link_limit: number,
+  link_order: BrokenLinksResultV1_BrokenLinkCheckerOptions_LinkOrder
+): LinkIntermediate[] {
+  // shuffle links if link_order is `RANDOM` and truncate to link_limit
+
+  // Shuffle the links if link_order is RANDOM, or copy the original array
+  const linksToFollow =
+    link_order === BrokenLinksResultV1_BrokenLinkCheckerOptions_LinkOrder.RANDOM
+      ? [...links]
+          .map((value) => ({ value, sort: Math.random() }))
+          .sort((a, b) => a.sort - b.sort)
+          .map(({ value }) => value)
+      : [...links];
+
+  // Truncate the processed array to match the link_limit
+  return linksToFollow.slice(0, link_limit! - 1);
+}
+
+export function getTimeLimitPromise(
+  startTime: string,
+  totalTimeoutMillis: number,
+  extraOffsetMillis = 0
+): [Promise<boolean>, NodeJS.Timeout, () => void] {
+  let timeLimitTimeout: NodeJS.Timeout;
+  let timeLimitresolver = () => {};
+  const timeLimitPromise = new Promise<boolean>((resolve) => {
+    timeLimitresolver = () => {
+      resolve(false);
+    };
+    const timeUsed = Date.now() - new Date(startTime).getTime();
+    timeLimitTimeout = setTimeout(
+      timeLimitresolver,
+      totalTimeoutMillis - timeUsed - extraOffsetMillis
+    );
+  });
+  return [timeLimitPromise, timeLimitTimeout!, timeLimitresolver!];
+}
+
+const getGenericError = (genericErrorMessage: string): GenericResultV1 => ({
+  ok: false,
+  generic_error: {
+    error_type: 'Error',
+    error_message: genericErrorMessage,
+    function_name: '',
+    file_path: '',
+    line: 0,
+    stack_trace: '',
+  },
+});
+
+export const getGenericSyntheticResult = (
+  startTime: string,
+  genericErrorMessage: string
+): SyntheticResult => ({
+  synthetic_generic_result_v1: getGenericError(genericErrorMessage),
+  runtime_metadata: getRuntimeMetadata(),
+  start_time: startTime,
+  end_time: new Date().toISOString(),
+});
---
+++ packages/synthetics-sdk-broken-links/test/unit/link_utils.spec.ts
@@ -0,0 +1,189 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { expect } from 'chai';
+import {
+  BrokenLinksResultV1_BrokenLinkCheckerOptions,
+  BrokenLinksResultV1_BrokenLinkCheckerOptions_LinkOrder,
+  BrokenLinksResultV1_SyntheticLinkResult,
+  ResponseStatusCode,
+  ResponseStatusCode_StatusClass,
+} from '@google-cloud/synthetics-sdk-api';
+import { LinkOrder, StatusClass } from '../../src/broken_links';
+import {
+  checkStatusPassing,
+  createSyntheticResult,
+  LinkIntermediate,
+  shuffleAndTruncate,
+} from '../../src/link_utils';
+import { setDefaultOptions } from '../../src/options_func';
+
+describe('GCM Synthetics Broken Links Utilies', async () => {
+  const status_value_200: ResponseStatusCode = { status_value: 200 };
+  const status_value_404: ResponseStatusCode = { status_value: 404 };
+  const status_class_1xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_1XX,
+  };
+  const status_class_2xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_2XX,
+  };
+  const status_class_3xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_3XX,
+  };
+  const status_class_4xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_4XX,
+  };
+  const status_class_5xx: ResponseStatusCode = {
+    status_class: ResponseStatusCode_StatusClass.STATUS_CLASS_5XX,
+  };
+
+  it('checkStatusPassing returns correctly when passed a number as ResponseStatusCode', () => {
+    // expecting success
+    expect(checkStatusPassing(status_value_200, 200)).to.be.true;
+    expect(checkStatusPassing(status_value_200, 404)).to.be.false;
+
+    // expecting failure
+    expect(checkStatusPassing(status_value_404, 200)).to.be.false;
+    expect(checkStatusPassing(status_value_404, 404)).to.be.true;
+  });
+
+  it('checkStatusPassing returns correctly when passed a statusClass as ResponseStatusCode', () => {
+    // expecting success
+    expect(checkStatusPassing(status_class_1xx, 100)).to.be.true;
+    expect(checkStatusPassing(status_class_2xx, 200)).to.be.true;
+    expect(checkStatusPassing(status_class_3xx, 304)).to.be.true;
+    expect(checkStatusPassing(status_class_4xx, 404)).to.be.true;
+    expect(checkStatusPassing(status_class_5xx, 504)).to.be.true;
+
+    // expecting failure
+    expect(checkStatusPassing(status_class_1xx, 200)).to.be.false;
+    expect(checkStatusPassing(status_class_2xx, 404)).to.be.false;
+    expect(checkStatusPassing(status_class_3xx, 200)).to.be.false;
+    expect(checkStatusPassing(status_class_4xx, 200)).to.be.false;
+    expect(checkStatusPassing(status_class_5xx, 200)).to.be.false;
+  });
+
+  it('createSyntheticResult correctly sets all aggregate fields in BrokenLinksResultV1', () => {
+    const origin_link = {
+      link_passed: true,
+      status_code: 200,
+      is_origin: true,
+    } as BrokenLinksResultV1_SyntheticLinkResult;
+
+    const followed_links = [
+      { link_passed: true, status_code: 200 },
+      { link_passed: true, status_code: 200 },
+      { link_passed: true, status_code: 200 },
+      { link_passed: true, status_code: 304 }, // eg of link specific setting
+      { link_passed: false, status_code: 404 },
+      { link_passed: false, status_code: null },
+      { link_passed: false, status_code: 505 },
+    ] as BrokenLinksResultV1_SyntheticLinkResult[];
+
+    const all_links = [origin_link, ...followed_links];
+    const start_time = new Date().toISOString();
+    const runtime_metadata = {
+      K_SERVICE: 'fake-service',
+      K_REVISION: 'fake-revision',
+    };
+    const options: BrokenLinksResultV1_BrokenLinkCheckerOptions =
+      setDefaultOptions({
+        origin_uri: 'https://example.com',
+        get_attributes: ['src'],
+        link_order: LinkOrder.RANDOM,
+        link_timeout_millis: 5000,
+        wait_for_selector: '.content',
+        per_link_options: {
+          'fake-link1': { expected_status_code: StatusClass.STATUS_CLASS_4XX },
+          'fake-link2': { expected_status_code: 304 },
+          'fake-link3': { link_timeout_millis: 10 },
+          'fake-link4': {
+            expected_status_code: StatusClass.STATUS_CLASS_3XX,
+            link_timeout_millis: 10,
+          },
+        },
+      });
+
+    const syntheticResult = createSyntheticResult(
+      start_time,
+      runtime_metadata,
+      options,
+      all_links
+    );
+
+    // BrokenLinkResultV1 expectations (testing `parseFollowedLinks`)
+    const broken_links_result =
+      syntheticResult.synthetic_broken_links_result_v1;
+    expect(broken_links_result).to.deep.equal({
+      link_count: 8,
+      passing_link_count: 5,
+      failing_link_count: 3,
+      unreachable_count: 1,
+      status2xx_count: 4,
+      status3xx_count: 1,
+      status4xx_count: 1,
+      status5xx_count: 1,
+      options: options,
+      origin_link_result: origin_link,
+      followed_link_results: followed_links,
+    });
+
+    expect(
+      new Date(syntheticResult.start_time).getTime()
+    ).to.be.lessThanOrEqual(new Date(syntheticResult.end_time).getTime());
+    expect(syntheticResult.runtime_metadata).to.not.be.undefined;
+  });
+
+  describe('shuffleAndTruncate', () => {
+    const links: LinkIntermediate[] = [
+      { target_uri: 'link1', html_element: '', anchor_text: '' },
+      { target_uri: 'link2', html_element: '', anchor_text: '' },
+      { target_uri: 'link3', html_element: '', anchor_text: '' },
+      { target_uri: 'link4', html_element: '', anchor_text: '' },
+      { target_uri: 'link5', html_element: '', anchor_text: '' },
+      { target_uri: 'link6', html_element: '', anchor_text: '' },
+      { target_uri: 'link7', html_element: '', anchor_text: '' },
+      { target_uri: 'link8', html_element: '', anchor_text: '' },
+    ];
+    const random =
+      BrokenLinksResultV1_BrokenLinkCheckerOptions_LinkOrder.RANDOM;
+    const firstN =
+      BrokenLinksResultV1_BrokenLinkCheckerOptions_LinkOrder.FIRST_N;
+
+    it('shuffles links when link_order is RANDOM', () => {
+      const link_limit = 10;
+
+      const shuffledLinks = shuffleAndTruncate(links, link_limit, random);
+      // Expect that the shuffledLinks array is not equal to the original links array
+      expect(shuffledLinks).to.not.deep.equal(links);
+    });
+
+    it('does not shuffle links when link_order is not RANDOM', () => {
+      const link_limit = 10;
+
+      const unshuffledLinks = shuffleAndTruncate(links, link_limit, firstN);
+      // Expect that the shuffledLinks array is equal to the original links array
+      expect(unshuffledLinks).to.deep.equal(links);
+    });
+
+    it('truncates to link_limit if less than the number of links', () => {
+      const link_limit = 3; // Less than the number of links
+
+      const truncatedLinks = shuffleAndTruncate(links, link_limit, firstN);
+      // Expect that the truncatedLinks array has a length equal to link_limit-1
+      // (this is to account for the origin_uri being included in link_limit)
+      expect(truncatedLinks).to.have.lengthOf(link_limit - 1);
+    });
+  });
+});
